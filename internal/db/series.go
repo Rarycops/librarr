62473fe141b31e972e7e0cbd5fd8d8686aba1090
Miguel <mj.rarycops@gmail.com>
2026-09-09 21:57:31 +0200

    Revert "Support expected manga volume totals"
    This reverts commit 5267847de3fe77f32ca56da728d96affa1db9f6a.

--- internal/db/series.go
+++ internal/db/series.go
@@ -51,14 +51,3 @@ func (d *DB) GetSeriesTracking() ([]map[string]interface{}, error) {
 	}
 	return result, nil
 }
-
-// SetSeriesTrackingTotal persists an operator-provided expected volume total.
-func (d *DB) SetSeriesTrackingTotal(seriesName string, knownTotal int) error {
-	d.mu.Lock()
-	defer d.mu.Unlock()
-	_, err := d.db.Exec(
-		"UPDATE series_tracking SET known_total=?, last_checked=? WHERE lower(series_name)=lower(?)",
-		knownTotal, float64(time.Now().Unix()), seriesName,
-	)
-	return err
-}
